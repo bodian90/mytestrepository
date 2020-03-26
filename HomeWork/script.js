0d63d5e392fd78fd28331066c8d142e18492d1e1
Bohdan <artuhov.bogdan@gmail.com>
2020-03-26 13:35:02 +0100

    copleted thrird practical lessons

--- HomeWork/script.js
+++ HomeWork/script.js
@@ -20,28 +20,6 @@ let appDate = {
     saving: true
 };
 
-// let i = 0;
-// while(i<2){
-//     i++;
-//      let a = prompt("Write obligatory expenses for this mounth", ''),
-//          b = prompt("How much is it?", '');
-//          if( (typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null
-//               && a != '' && b != '' && a.length < 50){
-//                  console.log("done");
-//                   appDate.expenses[a] = b;}
-// }
-
-// let i = 0;
-// do{
-//     i++;
-//     let a = prompt("Write obligatory expenses for this mounth", ''),
-//         b = prompt("How much is it?", '');
-//         if( (typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null
-//              && a != '' && b != '' && a.length < 50){
-//                  console.log("done");
-//                  appDate.expenses[a] = b;}
-// }while(i < 2);
-
 function chooseExpenses (){
     for(let i = 0; i < 2; i++) {
         let a = prompt("Write obligatory expenses for this mounth", ''),
@@ -53,33 +31,58 @@ function chooseExpenses (){
             appDate.expenses[a] = b;
             } else {
                 i = i - 1;
-            }   
-        };
+        }   
+    }
 }
+chooseExpenses();
 
-    appDate.moneyPerDay = appDate.budget/30;
-    alert("budger for a day " + appDate.moneyPerDay);
+function optionalExpenses() {
+    for(let i = 0; i < 3; i++) {
+        let option = prompt("Write optional obligatory for this mounth", ''),
+            expens = +prompt("How much is it?", '');
+    
+        // if( (typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null
+        // && a != '' && b != '' && a.length < 50){
+            
+            // } else {
+            //     i = i - 1;
+                console.log("done");
+            appDate.optionalExpenses[option] = expens;
+        }
+    }
+// }
+ optionalExpenses();
 
+function detectDayBudget(){
+    appDate.moneyPerDay = (appDate.budget/30).toFixed();
+        alert("budger for a day " + appDate.moneyPerDay);
+    }
+
+detectDayBudget();
+ 
+function detectLevel () {
     if (appDate.moneyPerDay < 100) {
         console.log('Minimals level of wealth');
-    } else if (appDate.moneyPerDay > 100 && appDate.moneyPerDay < 1000) {
+    } else if (appDate.moneyPerDay > 100 && appDate.moneyPerDay < 2000) {
         console.log("normal level of wealth");
-    } else if (appDate.moneyPerDay > 1000){
+    } else if (appDate.moneyPerDay > 2000){
         console.log("high level of wealth");
     }else {
         console.log("Eror");
     }
+}
+    detectLevel();
 
-    function checkSeving() {
-        if(appDate.saving == true){
-            let save = +prompt('How much of accumulation?')
-                percent = +prompt('What percentage?');
+function checkSeving() {
+     if(appDate.saving == true){
+        let save = +prompt('How much of accumulation?')
+             percent = +prompt('What percentage?');
 
-            appDate.mounthInCome = save/100*12*percent;
-            alert('Mouthly income in your deposite: ' + appDate.mounthInCome );
-                
+        appDate.mounthInCome = save/100/12*percent;
+        alert('Mouthly income in your deposite: ' + appDate.mounthInCome );  
              
         }
     }
 
     checkSeving();
+
